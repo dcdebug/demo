bbfc09c0cd790bce30c0de81e4ec5929fe9c948d
Darry Kinger <darrykinger@gmail.com>
2024-11-20 14:50:52 +0800

    add vpn

---
+++ docker-vpn/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+service tor start
+service privoxy start
+
+cp /etc/resolv.conf /tmp/resolv.conf
+su -c 'umount /etc/resolv.conf'
+cp /tmp/resolv.conf /etc/resolv.conf
+sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
+service expressvpn restart
+/usr/bin/expect /tmp/expressvpnActivate.sh
+expressvpn connect $SERVER
+exec "$@"
