4e856cb57024b20edef04689034c707b55a4c867
Ethan.Wong <darrykinger@gmail.com>
2024-12-30 15:49:45 +0800

    Create py.sh

---
+++ myssr/py.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt update && apt install supervisor ufw  -y  \
+&& cd /usr/local/src/ && wget https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20241227/gost_3.0.0-nightly.20241227_linux_amd64.tar.gz\
+&& tar -zxvf gost_3.0.0-nightly.20241227_linux_amd64.tar.gz && mv ./gost /usr/local/bin/\
+&& /usr/local/bin/gost -V \
+&& touch /etc/supervisor/conf.d/gost.conf \
+&& echo "[program:gost_http_proxy]
+command=/usr/local/bin/gost -L http://user:pass@:8080" > /etc/supervisor/conf.d/gost.conf\
+&& /etc/init.d/supervisor restart \
+&& sleep 6 && supervisorctl status \
+&& ufw allow 8080
