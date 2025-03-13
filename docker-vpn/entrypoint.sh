e01b3ba3fb01f23d58e924a3e8442330598db6eb
Darry Kinger <darrykinger@gmail.com>
2025-03-13 18:00:39 +0800

    <Fix> 	content: 	1. delete the launch of the tor and prixoy 	2. add the launch of the shadowsocks.

--- docker-vpn/entrypoint.sh
+++ docker-vpn/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-service tor start
-service privoxy start
-
+##service tor start
+##service privoxy start
+/etc/init.d/shadowsocks-libev start
 cp /etc/resolv.conf /tmp/resolv.conf
 su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
