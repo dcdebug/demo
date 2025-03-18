6ac13d348f16eac08257e14ab964ae8fc05a99ef
Darry Kinger <darrykinger@gmail.com>
2025-03-18 16:53:23 +0800

    add

--- docker-vpn/entrypoint.sh
+++ docker-vpn/entrypoint.sh
@@ -2,15 +2,7 @@
 
 ##service tor start
 ##service privoxy start
-echo '{ \
-"server":["::1", "0.0.0.0"], \
-"mode":"tcp_and_udp", \
-"server_port":8118, \
-"local_port":1080, \
-"password":"iserver123", \
-"timeout":86400, \
-"method":"chacha20-ietf-poly1305" \
-}' >  /etc/shadowsocks-libev/config.json && /etc/init.d/shadowsocks-libev start
+/etc/init.d/shadowsocks-libev start
 cp /etc/resolv.conf /tmp/resolv.conf
 su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
