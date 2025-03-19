6430763800671f7606a637faa6d1b8d2dd8b305d
Darry Kinger <darrykinger@gmail.com>
2025-03-19 11:56:30 +0800

    <Fix> 	content: 	echo config string to /etc/shadowsocks-libdev/config.json

--- docker-vpn/entrypoint.sh
+++ docker-vpn/entrypoint.sh
@@ -7,6 +7,21 @@ cp /etc/resolv.conf /tmp/resolv.conf
 su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
 sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
+
+
+cat <<EOF > /etc/shadowsocks-libev/config.json
+{
+    "server":["::1", "0.0.0.0"],
+    "mode":"tcp_and_udp",
+    "server_port":8118,
+    "local_port":1080,
+    "password":"iserver123",
+    "timeout":86400,
+    "method":"chacha20-ietf-poly1305"
+}
+EOF
+
+echo "配置文件已写入 /etc/shadowsocks-libev/config.json"
 service expressvpn restart
 /etc/init.d/shadowsocks-libev restart
 /usr/bin/expect /tmp/expressvpnActivate.sh
