39662d0757abf5f1fe1d5ea3f7751991abf89a71
Darry Kinger <darrykinger@gmail.com>
2025-03-19 11:25:33 +0800

    <fix> 	1. fix the build error 	2. add restart /etc/init.d/shadowsock-libdev

--- docker-vpn/entrypoint.sh
+++ docker-vpn/entrypoint.sh
@@ -2,12 +2,13 @@
 
 ##service tor start
 ##service privoxy start
-/etc/init.d/shadowsocks-libev start
+##/etc/init.d/shadowsocks-libev start
 cp /etc/resolv.conf /tmp/resolv.conf
 su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
 sed -i 's/DAEMON_ARGS=.*/DAEMON_ARGS=""/' /etc/init.d/expressvpn
 service expressvpn restart
+/etc/init.d/shadowsocks-libev restart
 /usr/bin/expect /tmp/expressvpnActivate.sh
 expressvpn connect $SERVER
 exec "$@"
